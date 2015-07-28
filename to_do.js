7e73880987f5839911881d8bd3e7cf46ba160d4b
scott <scottmacphersonmusic@gmail.com>
2015-07-28 16:52:12 -0700

    Add delete event for clicking on list items

--- to_do.js
+++ to_do.js
@@ -6,13 +6,26 @@ function newTask(e) {
   input = document.getElementById('add_task').children[0];
 
   new_task.appendChild(document.createTextNode(input.value));
-  task_list.appendChild(new_task);
+  task_list.insertBefore(new_task, task_list.childNodes[0]);
   input.value = '';
 }
 
+function deleteTask(e) {
+  var task_list, target;
+  task_list = document.getElementById('task_list');
+  target = e.target
+  task_list.removeChild(target);
+}
+
 function setup() {
-  var add_task_form = document.getElementById('add_task');
+  var add_task_form, task_list;
+  add_task_form = document.getElementById('add_task');
+  task_list = document.getElementById('task_list');
+
   add_task_form.addEventListener('submit', newTask, false);
+  task_list.addEventListener('click', function(e) {
+    deleteTask(e);
+  }, false);
 }
 
 window.addEventListener('DOMContentLoaded', setup, false);
